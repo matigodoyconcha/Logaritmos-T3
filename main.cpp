7ed818d5102ccdd30e4623711c1c136017acbf6e
Claudio Morel <claudio.morel@ug.uchile.cl>
2024-07-10 17:04:27 -0400

    Corregido el main y filtro de bloom terminado

--- main.cpp
+++ main.cpp
@@ -2,51 +2,59 @@
 #include <fstream>
 #include <vector>
 #include <string>
-#include <ctime>
+#include <chrono>
 #include <algorithm>
 #include "bloom_filter.h"
 
-std::vector<std::string> load_csv(const std::string &filename) {
-    std::vector<std::string> data;
-    std::ifstream file(filename);
-    std::string line;
-    while (std::getline(file, line)) {
+using namespace std;
+
+vector<string> load_csv(const string &filename) {
+    vector<string> data;
+    ifstream file(filename);
+    string line;
+    while (getline(file, line)) {
         data.push_back(line);
     }
     return data;
 }
 
 int main() {
-    const size_t bloom_filter_size = 10000;
+    const size_t bloom_filter_size = 93890*10;
     const int num_hashes = 7;
     BloomFilter bloom_filter(bloom_filter_size, num_hashes);
 
-    std::vector<std::string> baby_names = load_csv("Popular-Baby-Names.csv");
+    vector<string> baby_names = load_csv("Popular-Baby-Names-Final.csv");
     for (const auto &name : baby_names) {
         bloom_filter.add(name);
     }
 
-    std::vector<std::string> search_names = load_csv("Film-Names.csv");
-    std::clock_t start;
+    vector<string> search_names = load_csv("Film-Names.csv");
+    chrono::high_resolution_clock::time_point start;
     double duration;
 
     // Search without Bloom filter
-    start = std::clock();
+    start = chrono::high_resolution_clock::now();
     for (const auto &name : search_names) {
-        bool found = (std::find(baby_names.begin(), baby_names.end(), name) != baby_names.end());
+        bool found = (find(baby_names.begin(), baby_names.end(), name) != baby_names.end());
     }
-    duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
-    std::cout << "Search without Bloom filter took " << duration << " seconds." << std::endl;
+    duration = chrono::duration_cast<chrono::microseconds>(chrono::high_resolution_clock::now() - start).count();
+    cout << "Search without Bloom filter took " << duration << " microseconds." << endl;
 
     // Search with Bloom filter
-    start = std::clock();
+    start = chrono::high_resolution_clock::now();
+    long long count1 = 0;
+    long long count2 = 0;
     for (const auto &name : search_names) {
         if (bloom_filter.possibly_contains(name)) {
-            bool found = (std::find(baby_names.begin(), baby_names.end(), name) != baby_names.end());
+            bool found = (find(baby_names.begin(), baby_names.end(), name) != baby_names.end());
+            count1++;
+        }
+        else{
+            count2++;
         }
     }
-    duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
-    std::cout << "Search with Bloom filter took " << duration << " seconds." << std::endl;
+    duration = chrono::duration_cast<chrono::microseconds>(chrono::high_resolution_clock::now() - start).count();
+    cout << "Search with Bloom filter took " << duration << " microseconds." << endl;
 
     return 0;
 }
